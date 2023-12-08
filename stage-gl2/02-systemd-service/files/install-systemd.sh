f7297e2af0554a17dfa08cd488cb463bd8fae7f2
Leo Dirac <leo@groundlight.ai>
2023-12-08 01:00:28 +0000

    Undoing the substantive changes for MNS

--- stage-gl2/02-systemd-service/files/install-systemd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-sudo cp ./groundlight-mns.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable groundlight-mns
-
--- stage-gl2/02-systemd-service/files/service-up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /opt/groundlight/monitoring-notification-server/deploy/
-docker-compose up
-
