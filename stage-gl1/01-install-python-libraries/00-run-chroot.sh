b2b38e660e0a990f86e25b8ceb08e498aa95d57a
Leo Dirac <leo@groundlight.ai>
2023-12-13 00:05:29 +0000

    Using a python venv by default for groundlight python SDK stuff.

--- stage-gl1/01-install-python-libraries/00-run-chroot.sh
+++ stage-gl1/01-install-python-libraries/00-run-chroot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
-pipit() {
-    /usr/bin/pip3 $@ --break-system-packages
-}
+# set up a groundlight virtual environment for python
+python3 -m venv /opt/groundlight/gl-py
+chmod a+rw -R /opt/groundlight/gl-py
+source /opt/groundlight/gl-py/bin/activate
 
-pipit install groundlight
-# Framegrab will try to re-install opencv, which won't go well.
-pipit install --no-deps framegrab
+# Now install the groundlight python libraries
+pip install groundlight
+# framegrab will install opencv-python, numpy, and pillow
+pip install framegrab
+
+# add a .bashrc entry to activate the groundlight virtual environment
+echo "source /opt/groundlight/gl-py/bin/activate" >> /home/${FIRST_USER_NAME}/.bashrc
---
+++ stage-gl1/01-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+GL_DIR="${ROOTFS_DIR}/opt/groundlight"
+
+install -v files/gl-py-README.md             "${GL_DIR}/gl-py/README.md"
